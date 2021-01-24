35172b1c7826af58ab8765256685b995b685141e
Andreas <Hellberg.andreas@gmail.com>
2021-01-24 18:07:57 +0100

    submits on enter

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 ;(() => {
-  let user
   const submitButton = document.getElementById("submit")
   const inputElement = document.getElementById("input")
   const nameCard = document.getElementById("nameCard")
@@ -8,6 +7,7 @@
   const alternativeGreeting = document.getElementById("alternativeGreeting")
   const pictureButton = document.querySelectorAll(".pictureButton")
   const imageWrapper = document.getElementById("imageWrapper")
+  let user
 
   const setUserToDOM = () => {
     userElement.innerHTML = user
@@ -19,24 +19,29 @@
   const checkLocalStorage = (() => {
     if (localStorage.user) {
       user = localStorage.getItem("user")
-
       setUserToDOM()
       toggleHiddens()
       alternativeGreeting.innerHTML = "Nice to see you again"
     }
   })()
 
+  const setUserToLocalStorage = () => {
+    localStorage.setItem("user", user)
+    setUserToDOM()
+    toggleHiddens()
+  }
+
   submitButton.addEventListener("click", () => {
     if (user.length > 0) {
-      localStorage.setItem("user", user)
-      setUserToDOM()
-      toggleHiddens()
+      setUserToLocalStorage()
     }
   })
 
-  inputElement.addEventListener("input", (e) => {
+  inputElement.addEventListener("keyup", (e) => {
     user = e.target.value
-    return user
+    if (e.key === "Enter") {
+      setUserToLocalStorage()
+    }
   })
   pictureButton.forEach((button) => {
     button.addEventListener("click", () => {
